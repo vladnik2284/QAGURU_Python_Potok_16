4658d67cc38aa3e43f7584f25664fdf2479cbf49
Vladimir Nikolaev <vladnik2284@yandex.ru>
2025-04-17 21:20:00 +0400

    Ответы на Задание_4. Основы Python. Часть I

--- test_search.py
+++ test_search.py
@@ -11,3 +11,9 @@ def test_element_found(driver):
 def test_element_not_found(driver):
     driver.element('[role="search"] input').should(be.blank).type(QUERY_FAIL).press_enter()
     driver.element('div.EmptySearchResults').should(be.present)
+
+
+from selene import browser, be, have
+browser.open('https://google.com')
+browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
