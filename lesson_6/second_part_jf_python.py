480b5af96733b65b86164efb3559a0118ce1cfeb
Vladimir Nikolaev <vladnik2284@yandex.ru>
2025-05-19 16:40:39 +0400

    Добавил папку и первые две задачи

---
+++ lesson_6/second_part_jf_python.py
@@ -0,0 +1,32 @@
+from datetime import time
+
+
+def test_dark_theme_by_time():
+    """
+    Протестируйте правильность переключения темной темы на сайте в зависимости от времени
+    """
+    current_time = time(hour=23)
+    # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
+
+    is_dark_theme = None
+    is_dark_theme = time(hour=6) >= current_time or current_time > time(hour=22)
+    assert is_dark_theme is True
+
+
+def test_dark_theme_by_time_and_user_choice():
+    """
+    Протестируйте правильность переключения темной темы на сайте
+    в зависимости от времени и выбора пользователя
+    dark_theme_enabled_by_user = True - Темная тема включена
+    dark_theme_enabled_by_user = False - Темная тема выключена
+    dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
+    """
+    current_time = time(hour=16)
+    dark_theme_enabled_by_user = True
+    # TODO переключите темную тему в зависимости от времени суток,
+    #  но учтите что темная тема может быть включена вручную
+
+    is_dark_theme = dark_theme_enabled_by_user
+    if dark_theme_enabled_by_user is None:
+        is_dark_theme = time(hour=6) >= current_time or current_time > time(hour=22)
+    assert is_dark_theme is True
